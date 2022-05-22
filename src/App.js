72032d69a2ba1c9ef4af4cdef00f3f52c51a90c9
NeelPoros <sawkotalimahmud@gmail.com>
2022-05-21 23:02:55 -0700

    Navbar Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
+import { Route, Routes } from "react-router-dom";
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home";
+import Navbar from "./Pages/Sheard/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h2 className="text-3xl font-bold">Welcome To Sarah Enterprise</h2>
+    <div >
+      <Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+      </Routes>
+      </Navbar>
+      
     </div>
   );
 }
